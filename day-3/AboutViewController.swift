c0b2fc35c9eaac25cad4247731e0e27cc8763c74
MacStudent <macstudent@126-iMac07.local>
2019-03-07 21:12:19 -0500

    day 4 completed

--- day-3/AboutViewController.swift
+++ day-3/AboutViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var lbl2: UILabel!
     var c:String?
-     @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var seg1: UISegmentedControl!
+    @IBOutlet weak var lblslider: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+           seg1.insertSegment(withTitle: "Monkey", at: 3, animated: true)
 if let i=c
 {
         lbl1.text=i
@@ -33,6 +39,30 @@ if let i=c
     @IBAction func btn1(_ sender: UIButton) {
           let v = self.navigationController?.viewControllers[1]
         self.navigationController?.popToViewController(v!, animated: true)
+    }
+    
+    @IBAction func btnsegment(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            img1.image=UIImage(named: "0.jpeg")
+        case 1:
+            img1.image=UIImage(named: "1.jpeg")
+        case 2:
+            img1.image=UIImage(named: "2.jpeg")
+        case 3:
+             img1.image=UIImage(named: "3.jpeg")
+        default:
+             img1.image=UIImage(named: "0.jpeg")
+        }
+    }
+    
+    @IBAction func slider(_ sender: UISlider) {
+        lblslider.text=String(Int(sender.value))
+    }
+    
+    @IBAction func stepper1(_ sender: UIStepper) {
+        lbl2.text="\(sender.value)"
     }
     
 }
